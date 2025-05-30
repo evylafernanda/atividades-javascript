e21a108818f1425cd076b0e2ea355b20fec1c7aa
Evyla <evyllafernanda0@gmail.com>
2025-05-30 10:39:43 -0300

    Add files via upload

---
+++ atividade-03-console-prompt/atividade01.js
@@ -0,0 +1,9 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let cidade = prompt("Em qual cidade você mora?");
+
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Cidade:", cidade);
+
+//1️⃣ Criando as Perguntas e Armazenando as Respostas
---
+++ atividade-03-console-prompt/atividade02.js
@@ -0,0 +1,10 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let cidade = prompt("Em qual cidade você mora?");
+
+console.log("Suas respostas:");
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Cidade:", cidade);
+
+//2️⃣ Exibindo as Respostas no Console
